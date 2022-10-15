376f7a841d46e5d2ab957e648a60da05e78c6af5
kurokuro325 <katapa.zari.123@gmail.com>
2022-10-15 17:36:31 +0900

    モデルとコントローラーの作成

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,9 @@ class Tweet < ApplicationRecord
   validates :text, presence: true
   belongs_to :user
   has_many :comments
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 end
